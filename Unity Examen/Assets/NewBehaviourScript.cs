743ca62b4100bb77a616995205ef4ebfc265dfa0
AntonioGBStudent <antgbstudent@gmail.com>
2018-12-18 17:14:07 +0100

    Initial commit

---
+++ Unity Examen/Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public GameObject pelota;
+    public float velocidad;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            pelota.transform.Translate(0.56f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            pelota.transform.Translate(-0.56f, 0, 0);
+        }
+    }
+}
